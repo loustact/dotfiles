ea1ffc0ea209c5bf05a04737411faa03a4bbe7b2
Loustro <lou.stro@proton.me>
2026-01-17 22:27:00 +0800

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+-- Reload Config
+config.automatically_reload_config = true
+
+-- Disable Tabs When Only 1 is Present
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Disable Close Confirmation
+config.window_close_confirmation = 'NeverPrompt'
+
+-- Set Window Decorations (Disables title bar but keeps resizable border)
+-- config.window_decorations = 'RESIZE'
+
+-- Cursor Style
+config.default_cursor_style = 'BlinkingBar'
+
+-- Color Scheme
+config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+-- Font
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
+config.font_size = 10
+
+-- Specify Current Working Directory
+config.default_cwd = "~/"
+
+-- Window Size
+config.initial_rows = 30
+config.initial_cols = 100
+
+return config
